7eee261faa6e3f9031c4c0881161aa68198be411
nhub1910119 <nhub1910119@gmail.com>
2022-11-01 10:15:11 +0700

    Tao trang them moi mot lien he

--- src/router/index.js
+++ src/router/index.js
@@ -8,15 +8,20 @@ const routes = [
     component: ContactBook,
   },
   {
-    path: "/:pathMatch(.*)*",
-    name: "notfound",
-    component: () => import("@/views/NotFound.vue"),
-  },
-  {
     path: "/contacts/:id",
     name: "contact.edit",
     component: () => import("@/views/ContactEdit.vue"),
     props: true, // Truyền các biến trong $route.params vào làm props
+  },
+  {
+    path: "/contacts/",
+    name: "contact.add",
+    component: () => import("@/views/ContactCreate.vue"),
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
   },
 ];
 
